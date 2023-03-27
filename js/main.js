0669296c8518a980e577760ddcd4dcbfa03be92f
Nsv17 <svanadzenn@gmail.com>
2023-03-27 22:05:35 +0300

    module9-task2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { showPictureHandler } from './big-picture.js';
 import { initPhotoPostForm } from './form.js';
+import { resetScale } from './scale.js';
 
 showPictureHandler();
 initPhotoPostForm();
+resetScale();
